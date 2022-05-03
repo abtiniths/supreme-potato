99f41f30d9dd536cf6b31d0de3134256c235ff2d
abtiniths <abtin.nouran@iths.se>
2022-05-04 00:25:36 +0200

    redbull and task controllers added

--- controllers/task/index.js
+++ controllers/task/index.js
@@ -6,24 +6,48 @@ const {BadRequestError, NotFoundError} = require('../../errors')
 
 
 const getAllTasks = async (req, res) => {
-    
+    const tasks = await Task.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({ tasks })
 }
 
 const getTask = async (req, res) => {
-    res.send('get task')
+    const {user:{userId}, params:{id:taskId}} = req
+    const task = await Task.findOne({
+        _id:taskId,createdBy:userId
+    })
+    if(!task){
+        throw new NotFoundError(`no task with id ${taskId}`)
+    }
+    res.status(StatusCodes.OK).json({ task })
 }
 
 const createTask = async (req, res) => {
-   
-    
+    req.body.createdBy = req.user.userId
+    const task = await Task.create(req.body)
+    res.status(StatusCodes.CREATED).json({ task })
+
 }
 
 const updateTask = async (req, res) => {
-    res.send('update task')
+    const {body:{titel,done},user:{userId}, params:{id:taskId}} = req
+
+    const task = await Task.findByIdAndUpdate({_id:taskId, createdBy:userId}, req.body, {new:true, runValidators:true})
+    if(!task){
+        throw new NotFoundError(`no task with id ${taskId}`)
+    }
+    res.status(StatusCodes.OK).json({ task })
 }
 
 const deleteTask = async (req, res) => {
-    res.send('delete task')
+    const {user:{userId}, params:{id:taskId}} = req
+    const task = await Task.findByIdAndRemove({
+        _id:taskId,
+        createdBy:userId
+    })
+    if(!task){
+        throw new NotFoundError(`no task with id ${taskId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 
